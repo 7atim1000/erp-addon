802bbb1dc85da1edb9d05c10371fef552ce0f4b8
Hatim <7atim1000@gmail.com>
2026-01-31 03:39:12 -0800

    Update CORS origin to allow production URL

--- Erp-backend/app.js
+++ Erp-backend/app.js
@@ -24,7 +24,7 @@ connectCloudinary();
 // cors policy to unblock response
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173']
+    origin: ['https://erp-addon.vercel.app']
  }));
  
 //Middleware Parse incoming request in json format and cookie parser for cookies and token 
